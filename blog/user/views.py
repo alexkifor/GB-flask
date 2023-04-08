af9e21e6bfe5d14fbe427aab9cda72356b97922a
Aleksandr <8712060@gmail.com>
2023-04-08 18:56:00 +0300

    lession_3

--- blog/user/views.py
+++ blog/user/views.py
@@ -1,60 +1,55 @@
 from flask import Blueprint, render_template, redirect
+from flask_login import login_required
 from werkzeug.exceptions import NotFound
 
 user = Blueprint('user', __name__, url_prefix='/users', static_folder='../static')
 
 #USERS = ['John', 'Michail', 'Anna']
-USERS = {
-    1: {'name': 'John',
-        'surname': 'Smitt',
-        'age': 31,
-        'gender': 'male',
-        'country': 'Australia',
-        'login': 'John_S'
-        },
-    2: {'name': 'Michael',
-        'surname': 'Brown',
-        'age': 26,
-        'gender': 'male',
-        'country': 'USA',
-        'login': 'Michael_B'
-        },
-    3: {'name': 'Anna',
-        'surname': 'Filippova',
-        'age': 35,
-        'gender': 'female',
-        'country': 'Russia',
-        'login': 'Anna_F'
-        }
-}
+# USERS = {
+#     1: {'name': 'John',
+#         'surname': 'Smitt',
+#         'age': 31,
+#         'gender': 'male',
+#         'country': 'Australia',
+#         'login': 'John_S'
+#         },
+#     2: {'name': 'Michael',
+#         'surname': 'Brown',
+#         'age': 26,
+#         'gender': 'male',
+#         'country': 'USA',
+#         'login': 'Michael_B'
+#         },
+#     3: {'name': 'Anna',
+#         'surname': 'Filippova',
+#         'age': 35,
+#         'gender': 'female',
+#         'country': 'Russia',
+#         'login': 'Anna_F'
+#         }
+# }
 
 @user.route('/')
+@login_required
 def user_list():
+    from blog.models import User
+    users = User.query.all()
     return render_template(
         'users/list.html',
-        users=USERS,
+        users=users,
         )
 
 @user.route('/<int:pk>')
-def get_user(pk: int):
-    try:
-        user_name = USERS[pk]['name']
-    except KeyError:
-        #raise NotFound(f'User id {pk} not found')
-        return redirect('/users/')
-    user_name = USERS[pk]['name']
-    user_surname = USERS[pk]['surname']
-    user_age = USERS[pk]['age']
-    user_gender = USERS[pk]['gender']
-    user_country = USERS[pk]['country']
-    user_login = USERS[pk]['login']
+@login_required
+def profile(pk: int):
+    from blog.models import User
+    
+    _user = User.query.filter_by(id=pk).one_or_none()
+    if not _user:
+        raise NotFound(f"User #{pk} doesn't exist!")
+
     return render_template(
-        'users/details.html',
-        user_name=user_name,
-        user_surname=user_surname,
-        user_age=user_age,
-        user_gender=user_gender,
-        user_country=user_country,
-        user_login=user_login
+        'users/profile.html',
+        user=_user,
         )
 
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
-from blog.app import create_app
+from werkzeug.security import generate_password_hash
+from blog.app import create_app, db
+
 
 # if __name__ == '__main__':
 #     app.run(
@@ -7,3 +9,15 @@ from blog.app import create_app
 #     )
 
 app = create_app()
+
+@app.cli.command('init-db')
+def init_db():
+    db.create_all()
+
+@app.cli.command('create_users')
+def create_users():
+    from blog.models import User
+    db.session.add(
+        User(email='user_1@email.com', password=generate_password_hash('test123'))
+    )
+    db.session.commit()
