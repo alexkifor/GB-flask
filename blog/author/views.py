f747a60551c8c7206112fbe049a7ccb483bcd60c
Aleksandr <8712060@gmail.com>
2023-04-28 23:26:08 +0300

    lession_6_update

---
+++ blog/author/views.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template
+from blog.models import Author
+
+author = Blueprint('author', __name__, url_prefix="/authors", static_folder='../static')
+
+@author.route('/')
+def author_list():
+    authors = Author.query.all()
+    return render_template(
+        'authors/list.html',
+        authors=authors,
+        )
