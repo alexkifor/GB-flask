013e12dc2080043dd1d44970ba924a9d94e950b8
Aleksandr <8712060@gmail.com>
2023-04-29 10:53:48 +0300

    lession_6

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from blog.app import db
 from flask_login import UserMixin
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+from datetime import datetime
 
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
@@ -11,8 +14,31 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(255))
     is_staff = db.Column(db.Boolean(), default=False)
 
+    author = relationship('Author', uselist=False, back_populates='user')
+
     def __init__(self, email, first_name, last_name, password):
         self.email = email
         self.first_name = first_name
         self.last_name = last_name
         self.password = password
+
+class Author(db.Model):
+    __tablename__='authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey('users.id'), nullable=False)
+
+    user = relationship('User', back_populates='author')
+    article = relationship('Article', uselist=False, back_populates='author')
+
+class Article(db.Model):
+    __tablename__='articles'
+
+    id = db.Column(db.Integer, primary_key=True)
+    author_id = db.Column(db.Integer, ForeignKey('authors.id'), nullable=False)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+
+    author = relationship('Author', uselist=False, back_populates='article')
