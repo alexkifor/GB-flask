c985a057bd0dc2244cbbb5e2a641baacff1fa129
Aleksandr <8712060@gmail.com>
2023-05-15 22:44:11 +0300

    change article/views

--- blog/article/views.py
+++ blog/article/views.py
@@ -17,11 +17,11 @@ article = Blueprint('article', __name__, url_prefix='/articles', static_folder='
 @article.route('/', methods=['GET'])
 def article_list():
     articles: Article = Article.query.all()
-    # count_articles: Dict = requests.get(f"{os.environ.get('API_URI')}/api/articles/event_get_count/").json()
+    count_articles: Dict = requests.get(f"{os.environ.get('API_URI')}/api/articles/event_get_count/").json()
     return render_template(
         'articles/list.html',
         articles=articles,
-    #     count_articles=count_articles['count'],
+        count_articles=count_articles['count'],
      )
 
 
